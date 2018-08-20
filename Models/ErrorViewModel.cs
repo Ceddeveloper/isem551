ade6c8735c3cdf6db02be7d8ebd349c807968064
Ceddeveloper <chetankj@yahoo.com>
2018-08-20 14:54:48 -0400

    Add files via upload
    initial code

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ISEM_551_90_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
